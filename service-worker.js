6eebef70fdec8cfa5f0d5d24a883963e80e6328d
Connor Bell <cjbell630@users.noreply.github.com>
2021-01-29 00:40:05 -0500

    added caching to make it work offline (maybe)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,28 @@
+let cacheName = 'jcp-0-0-0-a-14';
+let appShellFiles = [
+    "index.html",
+    "res/json-contents.js",
+    "res/words.js",
+    "script/conjugation-parser.js",
+    "script/conjugator.js",
+    "script/util.js",
+    "app/images/icon-032.png",
+    "app/images/icon-064.png",
+    "app/images/icon-128.png",
+    "app/images/icon-192.png",
+    "app/images/icon-256.png",
+    "app/images/icon-512.png"
+];
+
 self.addEventListener('install', (event) => {
+    //https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
     console.log('Inside the install handler:', event);
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
 });
 
 self.addEventListener('activate', (event) => {
@@ -7,5 +30,18 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener(fetch, (event) => {
+    //https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
     console.log('Inside the fetch handler:', event);
+    event.respondWith(
+        caches.match(event.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: ' + event.request.url);
+            return r || fetch(event.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: ' + event.request.url);
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
