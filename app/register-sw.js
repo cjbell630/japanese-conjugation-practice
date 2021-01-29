5d9376a5f9e0d06b9111e5fa524adcae586410d8
Connor Bell <cjbell630@users.noreply.github.com>
2021-01-28 23:44:26 -0500

    dudint change service-worker.js path in register-sw.js

--- app/register-sw.js
+++ app/register-sw.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register("app/service-worker.js");
+        navigator.serviceWorker.register("service-worker.js");
     });
 }
