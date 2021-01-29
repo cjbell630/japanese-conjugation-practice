7c2145e35a289f17bc2d0fc2f2b3230be2e7c241
Connor Bell <cjbell630@users.noreply.github.com>
2021-01-29 17:17:28 -0500

    trying again

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-let cacheName = "v0.0.2 a 15";
+let cacheName = "v0.0.2 a 16";
 let appShellFiles = [
     "index.html",
     "manifest.webapp",
@@ -32,7 +32,7 @@ self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log("[Service Worker {" + cacheName + "}] Caching all: app shell and content");
-            window.location.reload(); // attempt
+            location.reload(); // attempt
             return cache.addAll(appShellFiles);
         })
     );
